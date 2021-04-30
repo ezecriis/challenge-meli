e9a16b86164d4e0dbd5717dad028fb6cf703c101
Cristian Ledesma <cr.ledesma@hotmail.com>
2021-04-30 17:10:51 -0300

    App & index

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Controllers
+const itemsRouter = require('./controllers/items');
+
+const middleware = require('./utils/middleware');
+
+const allowedDomain = 'http://localhost:3000';
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', `${allowedDomain}`);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use(middleware.requestLogger);
+
+app.use('/api/items', itemsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,14 @@
+/*
+ *   App entry point.
+ *   Hace un import la aplicación desde el archivo
+ *   app.js y luego inicia la aplicación.
+ */
+
+const config = require('./utils/config');
+const http = require('http');
+const app = require('./app');
+
+const server = http.createServer(app);
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
+});
