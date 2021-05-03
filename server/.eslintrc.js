5c4a87b319e93d596ea9f9782b8e7594fab59725
Cristian Ledesma <cr.ledesma@hotmail.com>
2021-05-03 11:29:10 -0300

    .gitignore & .eslint updated

---
+++ server/.eslintrc.js
@@ -0,0 +1,44 @@
+export default {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2021
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0
+    },
+    "parserOptions": {
+        "sourceType": "module"
+    }
+};
